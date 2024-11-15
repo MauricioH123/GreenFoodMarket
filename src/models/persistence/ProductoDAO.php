0a822fa5ef51f5f0134b580db635b0e16ddc7dd5
MauricioH123 <mauricioh12345678912@gmail.com>
2024-11-15 18:45:03 -0500

    mejora lista productos

--- src/models/persistence/ProductoDAO.php
+++ src/models/persistence/ProductoDAO.php
@@ -19,14 +19,14 @@ class ProductoDAO{
     }
 
     public function obtenerProductos(){
-        $query = "SELECT * FROM productos";
+        $query = "SELECT * FROM productos_tienda";
         $stmt = $this -> conn -> prepare($query);
         $stmt -> execute();
         $resultado = $stmt -> get_result();
         
         $productos = array();
         while($row = $resultado -> fetch_assoc()){
-            $productos[] = new Producto($row['id_producto'], $row['id_proveedor'],$row['Nombre_producto'], $row['precio_venta']);
+            $productos[] = new Producto($row['id_producto'], $row['nombre_proveedor'],$row['Nombre_producto'], $row['precio_venta']);
         }
         $stmt->close();
         return $productos;
