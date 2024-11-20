6963c02f9f6c514ee5493a1c0fd1a084e7e4a2f7
MauricioH123 <mauricioh12345678912@gmail.com>
2024-11-19 20:18:57 -0500

    eliminacion arreglo de validaciones con libreria

--- src/controllers/ClienteCreaController.php
+++ src/controllers/ClienteCreaController.php
@@ -2,8 +2,8 @@
 namespace App\Controllers;
 
 use App\Models\Logica\ClienteService;
-use App\Helpers\Validacion;
 use App\Views\ClienteCrearView;
+use Respect\Validation\Validator as v;
 
 class ClienteCreaController{
 
@@ -20,22 +20,22 @@ class ClienteCreaController{
     }
 
     public function crearCliente($nombre, $numero_celular, $correo, $direccion){
-        if(!Validacion::validarNombre($nombre)){
+        if(!v::stringType()->notEmpty()->validate($nombre)){
             $this -> view ->render("Error: El nombre del cliente es inválido");
             return;
         }
 
-        if(!Validacion::validarNumero($numero_celular)){
+        if(!v::positive()->validate($numero_celular)){
             $this -> view ->render("Error: El numero de celular es invalido");
             return;
         }
 
-        if(!Validacion::validarCorreo($correo)){
+        if(!v::email()->validate($correo)){
             $this -> view ->render("Error: El correo es invalido");
             return;
         }
 
-        if(!Validacion::validarNombre($direccion)){
+        if(!v::positive()->validate($direccion)){
             $this -> view ->render("Error: La direccion es invalida");
             return;
         }
