947dad0b13a25df735cdcc307f9388e3647905ee
MauricioH123 <mauricioh12345678912@gmail.com>
2024-11-24 15:22:49 -0500

    tercera parte de proveedores

--- src/models/logica/ProveedorService.php
+++ src/models/logica/ProveedorService.php
@@ -14,4 +14,12 @@ class ProveedorService{
     public function listaProveedore(){
         return $this -> proveerdorDAO ->obtenerProveedor();
     }
+
+    public function borrarProveedor($id_proveedor){
+        return $this -> proveerdorDAO ->eliminarProveedor($id_proveedor);
+    }
+
+    public function actualizarProveedor($id_proveedor, $nombre_proveedor){
+        return $this -> proveerdorDAO ->editarProveedor($id_proveedor, $nombre_proveedor);
+    }
 }
