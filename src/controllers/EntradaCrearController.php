7eed8e7cb6ff4e53d12d5fddb21e98c26c1c2b7c
MauricioH123 <mauricioh12345678912@gmail.com>
2024-11-26 00:02:50 -0500

    quinta parte de entrada = compras

--- src/controllers/EntradaCrearController.php
+++ src/controllers/EntradaCrearController.php
@@ -6,11 +6,11 @@ use App\Views\EntradaCrearView;
 use Respect\Validation\Validator as v;
 
 class EntradaCrearController{
-    private $entradaSercie;
+    private $entradaService;
     private $view;
 
     public function __construct(){
-        $this -> entradaSercie = new EntradaService();
+        $this -> entradaService = new EntradaService();
         $this -> view = new EntradaCrearView();
     }
 
@@ -19,6 +19,23 @@ class EntradaCrearController{
     }
 
     public function validarFormulario($id_producto, $cantidad_entrada, $precio_entrada, $fecha_entrada){
+        if(!v::positive()->validate($id_producto)){
+            return "El id del producto es invalido";
+        }
 
+        if(!v::positive()->validate($cantidad_entrada)){
+            return "La cantidad unitaria del producto es invalido";
+        }
+
+        if(!v::positive()->validate($precio_entrada)){
+            return "El precio del producto es invalido";
+        }
+
+        if(!v::date('Y-m-d')->validate($fecha_entrada)){
+            return "La fecha de ingreso  del producto es invalido";
+        }
+
+        $crearE = $this -> entradaService ->creacionDEntarda($id_producto, $cantidad_entrada, $precio_entrada, $fecha_entrada);
+        $this -> view ->render($crearE);
     }
 }
